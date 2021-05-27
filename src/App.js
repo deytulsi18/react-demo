92cb38d3fbcbe088086d73cd26f70da6e3e364cc
Tulsi Dey <70888802+deytulsi18@users.noreply.github.com>
2021-05-28 00:13:19 +0530

    added the main changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { FooterContainer } from './containers/footer';
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <>
+      <Navbar />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hello, I am TD</p>
+          <a
+            className="App-link"
+            href="https://deytulsi18.github.io/Portfolio/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Visit my website
         </a>
-      </header>
-    </div>
+        </header>
+      </div>
+      <FooterContainer/>
+    </>
   );
 }
 
---
+++ src/global-styles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        position: relative;
+        font-family: 'Nunito', sans-serif;
+        font-family: 'Fira Code', monospace;
+        // outline: 3px solid red;
+    }
+`
